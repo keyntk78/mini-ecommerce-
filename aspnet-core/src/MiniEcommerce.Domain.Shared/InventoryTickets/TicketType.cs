c70c564c12403829ed4bff4864e142e6f1567b75
Nguyễn Tuấn Kiệt <101924064+keyntk78@users.noreply.github.com>
2023-03-15 10:20:29 +0700

    Create Entity

---
+++ aspnet-core/src/MiniEcommerce.Domain.Shared/InventoryTickets/TicketType.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MiniEcommerce.InventoryTickets
+{
+    public enum TicketType
+    {
+        Import,
+        Export
+    }
+}
---
+++ aspnet-core/src/MiniEcommerce.Domain.Shared/Promotions/DiscountUnit.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MiniEcommerce.Promotions
+{
+    public enum DiscountUnit
+    {
+        MoneyAmount,
+        Percentage
+    }
+}
