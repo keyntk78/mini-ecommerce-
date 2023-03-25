108fe48e0df869e0a3c9bf71ecc95e6681db717e
Nguyễn Tuấn Kiệt <101924064+keyntk78@users.noreply.github.com>
2023-03-25 22:11:39 +0700

    Login logout

---
+++ angular/src/app/auth/login/login.component.ts
@@ -0,0 +1,65 @@
+import { REFRESH_TOKEN } from '../../shared/constants/keys.const';
+import { LoginRequestDto } from './../../shared/models/login-request.dto';
+import { Component, OnDestroy } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { LayoutService } from 'src/app/layout/service/app.layout.service';
+import { AuthService } from 'src/app/shared/services/auth.service';
+import { Subject, takeUntil } from 'rxjs';
+import { ACCESS_TOKEN } from 'src/app/shared/constants/keys.const';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styles: [
+    `
+      :host ::ng-deep .pi-eye,
+      :host ::ng-deep .pi-eye-slash {
+        transform: scale(1.6);
+        margin-right: 1rem;
+        color: var(--primary-color) !important;
+      }
+    `,
+  ],
+})
+export class LoginComponent implements OnDestroy {
+  private ngUnsubscribe = new Subject();
+  valCheck: string[] = ['remember'];
+
+  password!: string;
+
+  loginForm: FormGroup;
+
+  constructor(
+    public layoutService: LayoutService,
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+  ) {
+    this.loginForm = this.fb.group({
+      username: new FormControl('', Validators.required),
+      password: new FormControl('', Validators.required),
+    });
+  }
+
+  login() {
+    var request: LoginRequestDto = {
+      username: this.loginForm.controls['username'].value,
+      password: this.loginForm.controls['password'].value,
+    };
+
+    this.authService
+      .login(request)
+      .pipe(takeUntil(this.ngUnsubscribe))
+      .subscribe(res => {
+        localStorage.setItem(ACCESS_TOKEN, res.access_token);
+        localStorage.setItem(REFRESH_TOKEN, res.refresh_token);
+        this.router.navigate(['']);
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.ngUnsubscribe.next;
+    this.ngUnsubscribe.complete();
+  }
+}
--- angular/src/app/layout/app.layout.module.ts
+++ angular/src/app/layout/app.layout.module.ts
@@ -17,6 +17,7 @@ import { AppFooterComponent } from './app.footer.component';
 import { AppConfigModule } from './config/config.module';
 import { AppSidebarComponent } from './app.sidebar.component';
 import { AppLayoutComponent } from './app.layout.component';
+import { TieredMenuModule } from 'primeng/tieredmenu';
 
 @NgModule({
   declarations: [
@@ -40,6 +41,7 @@ import { AppLayoutComponent } from './app.layout.component';
     RippleModule,
     RouterModule,
     AppConfigModule,
+    TieredMenuModule,
   ],
   exports: [AppLayoutComponent],
 })
